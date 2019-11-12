3c4f100acf370d3d163c578573e5cc613545be50
Default user <gaboandrez99@gmail.com>
2019-11-12 23:37:42 +0000

    Se corrigió el código para que el método de Euler no de la solución correcta.

--- Euler.cpp
+++ Euler.cpp
@@ -19,12 +19,11 @@ int main(int argc, char **argv){
     float t;
     for(t=0; t<=T;t+=h){
         std::cout << t << "\t" << v_siguiente << "\t" << x_siguiente << std::endl;
-        x_siguiente = x_anterior + h*v_siguiente;
-        x_anterior = x_siguiente;
-        
         v_siguiente = v_anterior + h*f(x_anterior);
-        v_anterior = v_siguiente;
+        x_siguiente = x_anterior + h*v_anterior;
         
+        v_anterior = v_siguiente;
+        x_anterior = x_siguiente;
         
     }
     std::cout << t << "\t" << v_siguiente << "\t" << x_siguiente << std::endl;
--- makefile
+++ makefile
@@ -2,9 +2,9 @@ grafica.png : graficar.py data.dat Euler.x
 	python graficar.py
 
 data.dat : Euler.x
-	./Euler.x 20 > data.dat
+	./Euler.x 50 > data.dat
 Euler.x : Euler.cpp
 	c++ Euler.cpp -o Euler.x
 	
 clean :
-	rm -r *.x *.dat
+	rm -r *.x *.dat *.png
